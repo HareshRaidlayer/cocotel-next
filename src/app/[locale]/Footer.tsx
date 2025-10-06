e3b72fec08640aab4117f1a0164bb9ff26fc6712
HareshRaidlayer <haresh@raidlayer.com>
2025-10-06 17:39:44 +0530

    loading changes

--- src/app/[locale]/Footer.tsx
+++ src/app/[locale]/Footer.tsx
@@ -58,12 +58,12 @@ const Footer: React.FC<BottomProps> = ({ data }) => {
   const { logo, socials, contacts, navSections } = data;
 
   return (
-    <footer className="relative bg-gradient-to-t from-green-700 via-green-600 to-green-500 text-white mt-10">
+    <footer className="relative bg-gradient-to-t from-green-700 via-green-600 to-green-500 text-white mt-10 p-2 ">
       <div className="absolute inset-0 bg-black/10"></div>
       <div className="relative z-10 container mx-auto pt-10 pb-6">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-5 gap-8">
           {/* LEFT SIDE: Logo + Socials + Contacts */}
-          <div className="flex flex-col items-center md:items-start space-y-6 text-center md:text-left xl:col-span-1">
+          <div className="flex flex-col items-center md:items-start space-y-6 text-center md:text-left md:col-span-1">
             <p className="text-lg font-semibold text-shadow-c-lg">Stay Connected with Us</p>
             <div className="flex gap-5">
               {socials.map((social, idx) => {
